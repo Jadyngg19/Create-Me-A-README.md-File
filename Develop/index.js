32130df434ec27d51660a7795e3186df303b1db6
Jadyn Gomez <jadyngg19@gmail.com>
2023-04-20 19:29:04 -0400

    still trying to fix ENOENT error

--- Develop/index.js
+++ Develop/index.js
@@ -83,7 +83,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log("Putting README.md file together...");
-        writeToFile("./Develop/README.md", generateMarkdown({...responses}));
+        writeToFile("/Users/jadyn/Bootcamp/Create-Me-A-README.md-File/Develop/index.js", generateMarkdown({...responses}));
     });
 }
 
